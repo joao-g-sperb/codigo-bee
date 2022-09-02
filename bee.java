7f58241af71e50195db830c498beb72f9ec44cab
joao-g-sperb <112731927+joao-g-sperb@users.noreply.github.com>
2022-09-02 18:55:04 -0300

    Create bee.java

---
+++ bee.java
@@ -0,0 +1,10 @@
+public class bee{
+  public static void main(String args[]){
+    Scanner in = new Scanner(System.in);
+    int a = in.nextInt();
+    int b = in.nextInt();
+    int soma = a + b;
+    System.out.println(soma);
+    
+  }
+}
